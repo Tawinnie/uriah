d99b9e035133ee40929a74f73e6736f1f282bca4
Tawina <94627449+Tawinnie@users.noreply.github.com>
2022-12-07 19:37:13 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,7 +1,13 @@
 <?php
 //Database credentials 
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
-define("DATABASE", "shoppn");
+// define("SERVER", "localhost");
+// define("USERNAME", "root");
+// define("PASSWD", "");
+// define("DATABASE", "shoppn");
+
+define("SERVERNAME", "us-cdbr-east-06.cleardb.net");
+define("HOSTNAME", "b007cea1daedae");
+define("PASSWORD", "ece837a4");
+define("DATABASE", "heroku_dfa175800c23a8a");
+
 
